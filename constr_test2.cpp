ee5e17fc58ecf6d35ae08b4dee668c290084d8c2
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2012-03-12 00:37:22 +0100

    references

--- constr_test2.cpp
+++ constr_test2.cpp
@@ -1,3 +1,8 @@
+// demonstrating non-determinism of constructor execution order
+
+// http://stackoverflow.com/questions/8433484/c-static-initialization-vs-attribute-constructor/8433525#8433525
+// http://llvm.org/bugs/show_bug.cgi?id=11521
+
 #include <stdio.h>
 
 struct Foo { Foo() { printf("foo\n"); } };
