069ee3129fd6041d5dbabe5473c3410066f3001c
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2012-04-23 02:07:51 +0200

    brew fix unlinked

---
+++ brew-fix-unlinked.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/ruby
+# -*- coding: utf-8 -*-
+
+HOMEBREW_BREW_FILE = ENV['HOMEBREW_BREW_FILE'] = File.expand_path("/usr/local/bin/brew")
+
+require 'pathname'
+HOMEBREW_LIBRARY_PATH = Pathname.new("/usr/local/bin/brew").realpath.dirname.parent.join("Library/Homebrew").to_s
+$:.unshift(HOMEBREW_LIBRARY_PATH + '/vendor')
+$:.unshift(HOMEBREW_LIBRARY_PATH)
+require 'global'
+
+    %w[CACHE CELLAR LIBRARY_PATH PREFIX REPOSITORY].each do |e|
+      ENV["HOMEBREW_#{e}"] = Object.const_get "HOMEBREW_#{e}"
+    end
+
+  unlinked = HOMEBREW_CELLAR.children.reject do |rack|
+    if not rack.directory?
+      true
+    elsif not (HOMEBREW_REPOSITORY/"Library/LinkedKegs"/rack.basename).directory?
+      Formula.factory(rack.basename).keg_only? rescue nil
+    else
+      true
+    end
+  end.map{ |pn| pn.basename }
+
+
+  unlinked.each do |l|
+	puts "brew link #{l}"
+	system "brew", "link", l
+	exit 1 if $?.exitstatus != 0
+  end
+
+
