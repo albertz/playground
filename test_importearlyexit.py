2912d4640c75a9c144aa30831053318d66808ad8
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2012-09-07 02:31:53 +0200

    comments / cleanup

--- test_importearlyexit.py
+++ test_importearlyexit.py
@@ -5,6 +5,20 @@
 print "mod: start"
 
 def breakModule():
+	"""
+	Be careful using this! It manipulates the code string of the code object
+	of the caller frame! It will overwrite the next 3 bytecodes.
+	
+	There are a few expectations that this work:
+	 - inspect.currentframe().f_back must return the caller frame
+	 - id(code.co_code) must return the native address of the code string
+	 - the C-API PyString_AsString must be available and callable via ctypes
+	
+	Discussion: The bytecode for Python modules allows the op RETURN_VALUE.
+	This is what you see at the compiled bytecode at the end of a module.
+	I wonder why `return` is not allowed at the module source level.
+	A normal `return` in a module should just work.
+	"""
 	import inspect
 	frame = inspect.currentframe()
 	frame = frame.f_back # manipulate the caller frame
@@ -13,12 +27,12 @@ def breakModule():
 	import ctypes
 	ctypes.pythonapi.PyString_AsString.argtypes = (ctypes.c_void_p,)
 	ctypes.pythonapi.PyString_AsString.restype = ctypes.POINTER(ctypes.c_char)
-	
-	import dis
-	
+		
 	targetjumpptr = len(code.co_code) - 4 # LOAD_CONST+RETURN_VALUE at end
 	iptr = frame.f_lasti + 3 # this is a funccall, so advance by 3
+	assert iptr + 3 <= targetjumpptr # otherwise we don't have enough room
 	
+	import dis
 	codestr = ctypes.pythonapi.PyString_AsString(id(code.co_code))
 	codestr[iptr] = chr(dis.opmap["JUMP_ABSOLUTE"])
 	codestr[iptr+1] = chr(targetjumpptr & 255)
