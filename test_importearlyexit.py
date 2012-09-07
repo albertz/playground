3ddd508dafcf46a9fa4de2f8f65b4f490c6e741c
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2012-09-07 02:16:12 +0200

    test_importearlyexit.py hacked :)

--- test_importearlyexit.py
+++ test_importearlyexit.py
@@ -4,33 +4,24 @@
 
 print "mod: start"
 
-import dis
-cstr = []
-#cstr += [dis.opmap["POP_BLOCK"]]
-#cstr += [dis.opmap["JUMP_ABSOLUTE"],255,255]
-cstr += [dis.opmap["LOAD_CONST"], 0, 0]
-cstr += [dis.opmap["RETURN_VALUE"]]
-
-#flags = 0
-flags = 1 + 2 + 64 # that is the default if it is a local function
-
-import types
-c = types.CodeType(
-	0, # argcount
-	0, # nlocals
-	1, # stacksize
-	flags, # flags
-	"".join(map(chr, cstr)), # codestring
-	(None,), # constants
-	(), # names
-	(), # varnames
-	"<code>", # filename
-	"exit", # name
-	1, # firstlineno
-	"", # lnotab
-	)
-
-
-exec c
+if True: # check if we should skip module loading here
+	import inspect
+	frame = inspect.currentframe()
+	code = frame.f_code
+	
+	import ctypes
+	ctypes.pythonapi.PyString_AsString.argtypes = (ctypes.c_void_p,)
+	ctypes.pythonapi.PyString_AsString.restype = ctypes.POINTER(ctypes.c_char)
+	
+	import dis
+	
+	while True:
+		targetjumpptr = len(code.co_code) - 4 # LOAD_CONST+RETURN_VALUE at end
+		iptr = frame.f_lasti
+		
+		codestr = ctypes.pythonapi.PyString_AsString(id(code.co_code))
+		codestr[iptr] = chr(dis.opmap["JUMP_ABSOLUTE"])
+		codestr[iptr+1] = chr(targetjumpptr & 255)
+		codestr[iptr+2] = chr(targetjumpptr >> 8)
 
 print "mod: after early exit"
