13b29a31ec8cc930e0997b46b0d64602a1cf94aa
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2013-02-11 20:11:32 +0100

    testforkexec.py

--- testforkexec.py
+++ testforkexec.py
@@ -8,21 +8,24 @@ if "--fork" in sys.argv:
 	print "in fork"
 	argidx = sys.argv.index("--fork")
 	readend = int(sys.argv[argidx + 1])
+	readend = os.fdopen(readend, "r")
 	
-	print "read:", os.read(readend, 100)
+	print "read:", readend.read()
 	sys.exit(0)
 
 readend,writeend = os.pipe()
+readend = os.fdopen(readend, "r")
+writeend = os.fdopen(writeend, "w")
 pid = os.fork()
 
 if pid == 0: # child
-	os.close(writeend)
+	writeend.close()
 	print "child", os.getpid()
-	os.execv(sys.argv[0], sys.argv + ["--fork", str(readend)])
+	os.execv(sys.argv[0], sys.argv + ["--fork", str(readend.fileno())])
 	
 else: # parent
-	os.close(readend)
+	readend.close()
 	
-	os.write(writeend, "foo")
+	writeend.write("foo")
 	print "parent"
 	
