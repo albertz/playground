e3c92526a851dda091ca1e666b38cd4339bf3085
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2014-02-18 13:15:26 +0100

    playing aroud

--- test_NSAutoreleaseNoPool.mm
+++ test_NSAutoreleaseNoPool.mm
@@ -4,12 +4,19 @@
 #include <dlfcn.h>
 #include <stdio.h>
 #import <Foundation/Foundation.h>
+#include <mach-o/dyld.h>
 
 typedef void (*NSAutoreleaseNoPoolFunc) (void* obj);
 
 void foo() {
 	NSAutoreleaseNoPoolFunc __NSAutoreleaseNoPool = (NSAutoreleaseNoPoolFunc) dlsym(RTLD_DEFAULT, "__NSAutoreleaseNoPool");
-	printf("func: %p\n", __NSAutoreleaseNoPool);	
+	//void* p2 = NSAddressOfSymbol("__NSAutoreleaseNoPool");
+	const struct mach_header* img = NSAddImage("/System/Library/Frameworks/CoreFoundation.framework/Versions/A/CoreFoundation", NSADDIMAGE_OPTION_NONE);
+	NSSymbol sym = NSLookupSymbolInImage(img, "___NSAutoreleaseNoPool", NSLOOKUPSYMBOLINIMAGE_OPTION_BIND_NOW);
+	NSSymbol sym2 = NSLookupAndBindSymbol("___NSAutoreleaseNoPool");
+	void* p3 = 0;
+	_dyld_lookup_and_bind("___NSAutoreleaseNoPool", &p3, NULL);
+	printf("func: %p (%p) %p %p %p\n", __NSAutoreleaseNoPool, img, sym, sym2, p3);
 }
 
 id raiseNoPool() {
