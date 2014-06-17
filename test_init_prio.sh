64065cb6fe649762ed628c2896380bd9bd9a3214
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2014-06-17 15:37:47 +0200

    cleanup. fix for linux

--- test_init_prio.sh
+++ test_init_prio.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+CXX=${CXX:-c++}
 PREFIX=test_init_prio
 
 for f in a b main; do
-	$CXX ${PREFIX}_$f.cpp -c -o ${PREFIX}_$f.o
-	ar rucs ${PREFIX}_$f.a ${PREFIX}_$f.o 
+	a=${PREFIX}_$f.a
+	o=${PREFIX}_$f.o
+	cpp=${PREFIX}_$f.cpp
+	$CXX $cpp -c -o $o
+	ar rucs $a $o
 done
 
-$CXX ${PREFIX}_b.a ${PREFIX}_a.a ${PREFIX}_main.a -o ${PREFIX}.exe
+exe=${PREFIX}.exe
+$CXX -Wl,-\( ${PREFIX}_b.a ${PREFIX}_a.a ${PREFIX}_main.a -Wl,-\) -o $exe
 
 ./${PREFIX}.exe
