4cc6480383be63fa614d5fede9bd6c8911790bb5
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2015-03-25 11:43:56 +0100

    update better_exchook

--- better_exchook.py
+++ better_exchook.py
@@ -1,7 +1,29 @@
 
-# by Albert Zeyer, www.az2000.de
-# code under GPLv3+
-# 2011-04-15
+# Copyright (c) 2012, Albert Zeyer, www.az2000.de
+# All rights reserved.
+# file created 2011-04-15
+
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met: 
+# 
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer. 
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution. 
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 
 # This is a simple replacement for the standard Python exception handler (sys.excepthook).
 # In addition to what the standard handler does, it also prints all referenced variables
@@ -12,11 +34,21 @@
 
 import sys, os, os.path
 
+try:
+	unicode
+except NameError: # Python3
+	unicode = str   # Python 3 compatibility
+
+try:
+	raw_input
+except NameError: # Python3
+	raw_input = input
+
 def parse_py_statement(line):
 	state = 0
 	curtoken = ""
 	spaces = " \t\n"
-	ops = ".,;:+-*/%&=|(){}[]^<>"
+	ops = ".,;:+-*/%&!=|(){}[]^<>"
 	i = 0
 	def _escape_char(c):
 		if c == "n": return "\n"
@@ -90,19 +122,17 @@ def set_linecache(filename, source):
 	import linecache
 	linecache.cache[filename] = None, None, [line+'\n' for line in source.splitlines()], filename
 
-if "raw_input" not in globals():
-	raw_input = input
-
 def simple_debug_shell(globals, locals):
 	try: import readline
-	except: pass # ignore
+	except ImportError: pass # ignore
 	COMPILE_STRING_FN = "<simple_debug_shell input>"
 	while True:
 		try:
 			s = raw_input("> ")
-		except:
+		except (KeyboardInterrupt, EOFError):
 			print("breaked debug shell: " + sys.exc_info()[0].__name__)
 			break
+		if s.strip() == "": continue
 		try:
 			c = compile(s, COMPILE_STRING_FN, "single")
 		except Exception as e:
@@ -111,25 +141,37 @@ def simple_debug_shell(globals, locals):
 			set_linecache(COMPILE_STRING_FN, s)
 			try:
 				ret = eval(c, globals, locals)
-			except:
+			except (KeyboardInterrupt, SystemExit):
+				print("debug shell exit: " + sys.exc_info()[0].__name__)
+				break
+			except Exception:
 				print("Error executing %r" % s)
 				better_exchook(*sys.exc_info(), autodebugshell=False)
 			else:
 				try:
 					if ret is not None: print(ret)
-				except:
+				except Exception:
 					print("Error printing return value of %r" % s)
 					better_exchook(*sys.exc_info(), autodebugshell=False)
 		
-def debug_shell(user_ns, user_global_ns):
+def debug_shell(user_ns, user_global_ns, execWrapper=None):
 	ipshell = None
 	try:
-		from IPython.Shell import IPShellEmbed,IPShell
-		ipshell = IPShell(argv=[], user_ns=user_ns, user_global_ns=user_global_ns)
-	except: pass
+		import IPython
+		import IPython.terminal.embed
+		class DummyMod(object): pass
+		module = DummyMod()
+		module.__dict__ = user_global_ns
+		module.__name__ = "DummyMod"
+		ipshell = IPython.terminal.embed.InteractiveShellEmbed(
+			user_ns=user_ns, user_module=module)
+	except Exception: pass
+	else:
+		if execWrapper:
+			old = ipshell.run_code
+			ipshell.run_code = lambda code: execWrapper(lambda: old(code))
 	if ipshell:
-		#ipshell()
-		ipshell.mainloop()
+		ipshell()
 	else:
 		simple_debug_shell(user_global_ns, user_ns)						
 
@@ -146,7 +188,7 @@ def pp_extra_info(obj, depthlimit = 3):
 				pass # don't print len in this case
 			else:
 				s += ["len = " + str(obj.__len__())]
-		except: pass
+		except Exception: pass
 	if depthlimit > 0 and hasattr(obj, "__getitem__"):
 		try:
 			if type(obj) in (str,unicode):
@@ -156,7 +198,7 @@ def pp_extra_info(obj, depthlimit = 3):
 				extra_info = pp_extra_info(subobj, depthlimit - 1)
 				if extra_info != "":
 					s += ["_[0]: {" + extra_info + "}"]
-		except: pass
+		except Exception: pass
 	return ", ".join(s)
 	
 def pretty_print(obj):
@@ -175,10 +217,18 @@ def fallback_findfile(filename):
 	if altfn[-4:-1] == ".py": altfn = altfn[:-1] # *.pyc or whatever
 	return altfn
 
-def better_exchook(etype, value, tb, debugshell=False, autodebugshell=True):
-	output("EXCEPTION")
-	output('Traceback (most recent call last):')
-	allLocals,allGlobals = {},{}
+def print_traceback(tb=None, allLocals=None, allGlobals=None):
+	if tb is None:
+		try:
+			tb = sys._getframe()
+			assert tb
+		except Exception:
+			output("print_traceback: tb is None and sys._getframe() failed")
+			return
+	import inspect
+	isframe = inspect.isframe
+	if isframe(tb): output('Traceback (most recent call first)')
+	else: output('Traceback (most recent call last):') # expect traceback-object (or compatible)
 	try:
 		import linecache
 		limit = None
@@ -198,10 +248,12 @@ def better_exchook(etype, value, tb, debugshell=False, autodebugshell=True):
 			except Exception as e:
 				return prefix + "!" + e.__class__.__name__ + ": " + str(e)
 		while _tb is not None and (limit is None or n < limit):
-			f = _tb.tb_frame
-			allLocals.update(f.f_locals)
-			allGlobals.update(f.f_globals)
-			lineno = _tb.tb_lineno
+			if isframe(_tb): f = _tb
+			else: f = _tb.tb_frame
+			if allLocals is not None: allLocals.update(f.f_locals)
+			if allGlobals is not None: allGlobals.update(f.f_globals)
+			if hasattr(_tb, "tb_lineno"): lineno = _tb.tb_lineno
+			else: lineno = f.f_lineno
 			co = f.f_code
 			filename = co.co_filename
 			name = co.co_name
@@ -220,7 +272,7 @@ def better_exchook(etype, value, tb, debugshell=False, autodebugshell=True):
 				alreadyPrintedLocals = set()
 				for tokenstr in grep_full_py_identifiers(parse_py_statement(line)):
 					splittedtoken = tuple(tokenstr.split("."))
-					for token in map(lambda i: splittedtoken[0:i], range(1, len(splittedtoken) + 1)):
+					for token in [splittedtoken[0:i] for i in range(1, len(splittedtoken) + 1)]:
 						if token in alreadyPrintedLocals: continue
 						tokenvalue = None
 						tokenvalue = _trySet(tokenvalue, "<local> ", lambda: pretty_print(_resolveIdentifier(f.f_locals, token)))
@@ -232,7 +284,8 @@ def better_exchook(etype, value, tb, debugshell=False, autodebugshell=True):
 				if len(alreadyPrintedLocals) == 0: output("       no locals")
 			else:
 				output('    -- code not available --')
-			_tb = _tb.tb_next
+			if isframe(_tb): _tb = _tb.f_back
+			else: _tb = _tb.tb_next
 			n += 1
 
 	except Exception as e:
@@ -240,12 +293,22 @@ def better_exchook(etype, value, tb, debugshell=False, autodebugshell=True):
 		import traceback
 		for l in traceback.format_exc().split("\n"): output("   " + l)
 		output("simple traceback:")
-		traceback.print_tb(tb)
+		if isframe(tb): traceback.print_stack(tb)
+		else: traceback.print_tb(tb)
+	
 
+def better_exchook(etype, value, tb, debugshell=False, autodebugshell=True):
+	output("EXCEPTION")
+	allLocals,allGlobals = {},{}
+	if tb is not None:
+		print_traceback(tb, allLocals=allLocals, allGlobals=allGlobals)
+	else:
+		output("better_exchook: traceback unknown")
+	
 	import types
 	def _some_str(value):
 		try: return str(value)
-		except: return '<unprintable %s object>' % type(value).__name__
+		except Exception: return '<unprintable %s object>' % type(value).__name__
 	def _format_final_exc_line(etype, value):
 		valuestr = _some_str(value)
 		if value is None or not valuestr:
@@ -262,7 +325,7 @@ def better_exchook(etype, value, tb, debugshell=False, autodebugshell=True):
 
 	if autodebugshell:
 		try: debugshell = int(os.environ["DEBUG"]) != 0
-		except: pass
+		except Exception: pass
 	if debugshell:
 		output("---------- DEBUG SHELL -----------")
 		debug_shell(user_ns=allLocals, user_global_ns=allGlobals)
@@ -280,13 +343,13 @@ if __name__ == "__main__":
 			y = "foo"
 			x, 42, sys.stdin.__class__, sys.exc_info, y, z
 		f()
-	except:
+	except Exception:
 		better_exchook(*sys.exc_info())
 
 	try:
 		f = lambda x: None
 		f(x, y)
-	except:
+	except Exception:
 		better_exchook(*sys.exc_info())
 
 	# use this to overwrite the global exception handler
