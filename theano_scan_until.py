06a6a1cf4a996eb74c8a97a9ff927d6566ac7039
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2016-04-06 17:08:12 +0200

    testing theano unroll scan

--- theano_scan_until.py
+++ theano_scan_until.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python
 
+import sys
 import theano
 import theano.tensor as T
 import numpy
+import better_exchook
+better_exchook.install()
 
 def step(i, s_p):
 	return [i], {}, theano.scan_module.until(T.eq(i, 5))
 
-s, _ = theano.scan(step, sequences=T.arange(10), outputs_info=[numpy.int64(0)])
+theano_scan = theano.scan
+
+if int((sys.argv + [0])[1]):
+	# Path to RETURNN. See homepage of RWTH i6 chair.
+	sys.path += ["/u/zeyer/setups/switchboard/2016-01-28--crnn/crnn"]
+	import TheanoUtil
+	theano_scan = TheanoUtil.unroll_scan
+
+s, _ = theano_scan(step, n_steps=10, sequences=T.arange(10), outputs_info=[numpy.int64(0)])
 print s.eval()
 
