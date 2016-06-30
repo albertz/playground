e4724d98f0761c7853a3b48ec419cdf9abf141c5
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2016-06-30 16:53:21 +0200

    hacking around

--- py_class_namespace.py
+++ py_class_namespace.py
@@ -1,6 +1,16 @@
+
+# http://stackoverflow.com/questions/38126674/python-how-to-access-outer-class-namespaces
+
+import sys
+
 class A:
 	class B:
 		a = 1
 	class C:
-		a = B.a  # wont find `B` here
+		print("C locals: %r" % locals())
+		print("C parent locals: %r" % sys._getframe(1).f_locals)
+		#a = B.a  # wont find `B` here
+		_B = sys._getframe(1).f_locals["B"]
+		a = _B.a
 
+print("A.C.a = %r" % A.C.a)
