00372ce548015247b2e10bf9ef0d922b924cfce7
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2017-10-20 14:02:01 +0200

    atfork wrapper, cleanup, small fix

--- atfork_patch_extended.c
+++ atfork_patch_extended.c
@@ -1,6 +1,9 @@
 /*
 
 See atfork_demo_main.c.
+And:
+https://stackoverflow.com/questions/46845496/ld-preload-and-linkage/
+
 Compile this:
 gcc -shared -fPIC atfork_patch_extended.c -o atfork_patch_extended.so
 
@@ -12,14 +15,9 @@ gcc -shared -fPIC atfork_patch_extended.c -o atfork_patch_extended.so
 #include <sys/syscall.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <dlfcn.h>
 #include <unistd.h>
 #include <string.h>
 
-
-void* (*real_dlsym)(void *handle, const char *symbol);
-void* (*real_dlopen)(const char *, int flag);
-extern void *_dl_sym (void *handle, const char *name, void *who);
 
 
 int pthread_atfork(void (*prepare)(void), void (*parent)(void), void (*child)(void)) {
@@ -28,41 +26,18 @@ int pthread_atfork(void (*prepare)(void), void (*parent)(void), void (*child)(vo
     return 0;
 }
 
-
-int _pthread_atfork(void (*prepare)(void), void (*parent)(void), void (*child)(void)) {
-    printf("Ignoring _pthread_atfork call extended!\n");
+int __register_atfork(void (*prepare)(void), void (*parent)(void), void (*child)(void)) {
+    printf("Ignoring __register_atfork call extended!\n");
     fflush(stdout);
     return 0;
-}
-
-int __pthread_atfork(void (*prepare)(void), void (*parent)(void), void (*child)(void)) {
-    printf("Ignoring __pthread_atfork call extended!\n");
-    fflush(stdout);
-    return 0;
-}
-
-
-void *dlsym(void *handle, const char *symbol) { 
-    printf("dlsym: %s\n", symbol);
-    void* result = real_dlsym(handle, symbol);
-    //if ((handle != RTLD_NEXT) || (handle != RTLD_DEFAULT))
-    if(strcmp(symbol, "pthread_atfork") == 0) { 
-        printf("Return our pthread_atfork\n");
-        return pthread_atfork;
-    }
-    return result; 
-}
-
-void *dlopen(const char *filename, int flag) {
-    printf("dlopen: %s\n", filename ? filename : "(NULL)");
-    int new_flag = flag & (~RTLD_DEEPBIND);
-    return (*real_dlopen)(filename, new_flag);
 }
 
 
 // Another way to ignore atfork handlers: Override fork.
 pid_t fork(void) {
-  return syscall(SYS_clone, SIGCHLD, 0);
+    printf("Patched fork().\n");
+    fflush(stdout);
+    return syscall(SYS_clone, SIGCHLD, 0);
 }
 
 
@@ -70,6 +45,4 @@ __attribute__((constructor))
 static void init(void) {
     printf("atfork_patch_extended init\n");
     fflush(stdout);
-    real_dlsym = _dl_sym(RTLD_NEXT, "dlsym", dlsym);
-    real_dlopen = dlsym(RTLD_NEXT, "dlopen");
 }
