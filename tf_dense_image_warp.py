58ef16775eadeea77a09be910c99497d9d4e9c9c
Albert Zeyer <albzey@gmail.com>
2019-06-08 16:43:04 +0200

    transformer

--- tf_dense_image_warp.py
+++ tf_dense_image_warp.py
@@ -29,9 +29,19 @@ class Transformer:
     self.session = tf.Session()
     self.img_in = tf.placeholder(tf.float32, shape=(None, None, 3), name="img_in")  # (width,height,channel)
     self.img_ins = tf.expand_dims(self.img_in, axis=0)  # (batch,width,height,channel)
-    # self.img_out = dense_image_warp()
-    self.img_outs = self.img_ins
+    self.flow = self._create_flow(shape=tf.shape(self.img_ins)[:-1], scale=5.)
+    self.img_outs = dense_image_warp(self.img_ins, flow=self.flow)
     self.img_out = tf.squeeze(self.img_outs, axis=0)  # (width,height,channel)
+
+  def _create_flow(self, shape, scale):
+    """
+    :param shape:
+    :param scale:
+    :return:
+    """
+    # [batch, height, width, 2]
+    flow = tf.random.normal(shape=tf.concat([shape, [2]], axis=0), stddev=scale)
+    return flow
 
   def transform(self, image):
     """
@@ -43,12 +53,16 @@ class Transformer:
 
 
 class Gui:
-  def __init__(self):
+  def __init__(self, transformer):
+    """
+    :param Transformer transformer:
+    """
     from kivy.app import App
     from kivy.uix.widget import Widget
     from kivy.graphics.texture import Texture
     from kivy.graphics import Rectangle
 
+    self.transformer = transformer
     self.image = load_image()
     self.app = App()
     self.app.root = Widget()
@@ -56,20 +70,35 @@ class Gui:
     size = tuple(numpy.array(self.image.shape[:2]).transpose())
     print("image size:", size)
 
-    texture = Texture.create(size=size, colorfmt="rgb", bufferfmt="float")
-    texture.blit_buffer(self.image.transpose([1, 0, 2]).tostring(), colorfmt="rgb", bufferfmt="float")
+    self.texture = Texture.create(size=size, colorfmt="rgb", bufferfmt="float")
+    self._blit(self.image)
 
     with self.app.root.canvas:
-      Rectangle(texture=texture, pos=(0, 0), size=numpy.array(size) * 2)
+      Rectangle(texture=self.texture, pos=(0, 0), size=numpy.array(size) * 2)
 
   def run(self):
+    from kivy.clock import Clock
+    Clock.schedule_interval(self.callback, 1.)
     self.app.run()
+
+  def _blit(self, img):
+    """
+    :param numpy.ndarray image: (width,height,channel=3), float32
+    """
+    assert img.ndim == 3 and img.shape[-1] == 3 and img.dtype == numpy.float32
+    self.texture.blit_buffer(img.transpose([1, 0, 2]).tostring(), colorfmt="rgb", bufferfmt="float")
+
+  def callback(self, dt):
+    img = self.transformer.transform(self.image)
+    self._blit(img)
+    self.app.root.canvas.ask_update()
 
 
 def main():
   import better_exchook
   better_exchook.install()
-  gui = Gui()
+  transformer = Transformer()
+  gui = Gui(transformer=transformer)
   gui.run()
 
 
