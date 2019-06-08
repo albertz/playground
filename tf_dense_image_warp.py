26765739cbed2f69b5039870b067d407dfa699a5
Albert Zeyer <albzey@gmail.com>
2019-06-08 17:16:32 +0200

    debug

--- tf_dense_image_warp.py
+++ tf_dense_image_warp.py
@@ -24,20 +24,60 @@ def load_image():
   return img
 
 
+def gaussian_kernel_2d(size, std):
+  """
+  :param int size:
+  :param float std:
+  :return: (size,size), float32
+  :rtype: tf.Tensor
+  """
+  d = tf.distributions.Normal(0.0, std)
+  values = d.prob(tf.range(start=-size, limit=size + 1, dtype=tf.float32))
+  values = tf.einsum('i,j->ij', values, values)
+  values.set_shape((size * 2 + 1, size * 2 + 1))
+  return values / tf.reduce_sum(values)
+
+
+def blur(image, size=None, std=None):
+  """
+  :param tf.Tensor image: (batch,width,height,channel)
+  :param int size:
+  :param float std:
+  :return: image
+  :rtype: tf.Tensor
+  """
+  if std is None:
+    std = 1.
+  if size is None:
+    size = int(std * 2 + 1)
+  image.set_shape((None, None, None, None))
+  orig_shape = tf.shape(image)
+  image = tf.transpose(image, [0, 3, 1, 2])  # (B,C,W,H)
+  image = tf.reshape(image, [orig_shape[0] * orig_shape[3], orig_shape[1], orig_shape[2], 1])  # (B*C,W,H,1)
+  gauss_kernel = gaussian_kernel_2d(size=size, std=std)
+  gauss_kernel = gauss_kernel[:, :, tf.newaxis, tf.newaxis]
+  image = tf.nn.conv2d(image, gauss_kernel, strides=[1, 1, 1, 1], padding="SAME")
+  image = tf.reshape(image, [orig_shape[0], orig_shape[3], orig_shape[1], orig_shape[2]])  # (B,C,W,H)
+  image = tf.transpose(image, [0, 2, 3, 1])  # (B,W,H,C)
+  return image
+
+
 class Transformer:
   def __init__(self):
     self.session = tf.Session()
     self.img_in = tf.placeholder(tf.float32, shape=(None, None, 3), name="img_in")  # (width,height,channel)
     self.img_ins = tf.expand_dims(self.img_in, axis=0)  # (batch,width,height,channel)
-    self.flow = self._create_flow(shape=tf.shape(self.img_ins)[:-1], scale=10.)
-    self.img_outs = dense_image_warp(self.img_ins, flow=self.flow)
+    self.flow = self._create_flow(shape=tf.shape(self.img_ins)[:-1], scale=50.)
+    # self.img_outs = dense_image_warp(self.img_ins, flow=self.flow)
+    self.img_outs = self._flow_x_to_img(self.flow)  # debug, to visualize the flow
     self.img_out = tf.squeeze(self.img_outs, axis=0)  # (width,height,channel)
 
   def _create_flow(self, shape, scale=None):
     """
     :param shape:
     :param scale:
-    :return:
+    :return: [batch, height, width, 2]
+    :rtype: tf.Tensor
     """
     scale_x = scale
     scale_y = scale
@@ -46,7 +86,18 @@ class Transformer:
     flow2 = tf.random.normal(shape=shape, stddev=scale_y)
     flow = tf.concat([tf.expand_dims(flow1, axis=-1), tf.expand_dims(flow2, axis=-1)], axis=-1)
     flow.set_shape((None, None, None, 2))
+    flow = blur(flow, std=5.)
     return flow
+
+  def _flow_x_to_img(self, flow):
+    """
+    :param tf.Tensor flow:
+    :return: [batch, height, width, 3]
+    :rtype: tf.Tensor
+    """
+    flow.set_shape((None, None, None, 2))
+    flow = flow[:, :, :, :1]
+    return tf.concat([flow, flow, flow], axis=-1)
 
   def transform(self, image):
     """
