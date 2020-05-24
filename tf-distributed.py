f991e6fe5ed09f7051b3a3006a403e86e2b8e28f
Albert Zeyer <albzey@gmail.com>
2020-05-24 17:54:09 +0200

    playing with distributed TensorFlow, multiprocessing

--- tf-distributed.py
+++ tf-distributed.py
@@ -8,22 +8,30 @@ https://stackoverflow.com/questions/61986820/tf-distribute-server-pipe-creation-
 
 import tensorflow as tf
 import better_exchook
+import multiprocessing
 
 better_exchook.install()
 tf.compat.v1.disable_eager_execution()
-print("TF:", tf.version.VERSION)
+
+cluster_def = {
+  'worker': ["localhost:12345", "localhost:23456"]
+}
+# task_type in {"chief", "worker"}
+# task_type == job_name (?)
+# worker_device = "/job:%s/task:%d" % (task_type, task_id)
+
+
+def proc_server(task_index: int):
+  server = tf.distribute.Server(cluster_def, task_index=task_index)
+  server.join()
 
 
 def main():
-  cluster_def = {
-    'worker': ["localhost:12345", "localhost:23456"]
-  }
-  # task_type in {"chief", "worker"}
-  # task_type == job_name (?)
-  # worker_device = "/job:%s/task:%d" % (task_type, task_id)
-  s1 = tf.distribute.Server(cluster_def, task_index=0)
-  s2 = tf.distribute.Server(cluster_def, task_index=1)
-  s1.join()
+  print("TF:", tf.version.VERSION)
+  multiprocessing.set_start_method("spawn")
+  proc = multiprocessing.Process(target=proc_server, args=(0,), daemon=True)
+  proc.start()
+  proc.join()
 
 
 if __name__ == '__main__':
