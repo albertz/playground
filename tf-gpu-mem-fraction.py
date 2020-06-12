c2a4794372abd3576da03c27bcb93e0866c371d2
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2020-06-12 10:36:10 +0200

    reproduce mem bug
    https://github.com/rwth-i6/returnn/issues/300

--- tf-gpu-mem-fraction.py
+++ tf-gpu-mem-fraction.py
@@ -2,32 +2,25 @@
 
 """
 Via: https://github.com/rwth-i6/returnn/issues/300
-By: Akshat Dewan <https://github.com/akshatdewan>
+Reported by Akshat Dewan <https://github.com/akshatdewan>.
 """
 
 import tensorflow as tf
-from numpy.random import default_rng
+from tensorflow.python.client import device_lib
 import time
 
 tf.compat.v1.disable_eager_execution()
 
+v = tf.compat.v1.get_variable("var", shape=(100, 100))
+result = v
 
-rng = default_rng()
-vals = rng.standard_normal((100,100))
-more_vals = rng.standard_normal((100,100))
+# The following line will cause the full GPU mem allocation.
+print(list(device_lib.list_local_devices()))
 
-# Initialize two constants
-x1 = tf.constant(vals)
-x2 = tf.constant(more_vals)
-
-# Multiply
-result = tf.multiply(x1, x2)
-
-# Assume that you have 12GB of GPU memory and want to allocate ~4GB:
 gpu_options = tf.compat.v1.GPUOptions(per_process_gpu_memory_fraction=0.1)
 
 with tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(gpu_options=gpu_options)) as sess:
-# Initialize Session and run `result`
+  sess.run(tf.compat.v1.global_variables_initializer())
   output = sess.run(result)
   print(output)
 
