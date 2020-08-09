10e09bb7f1ebf28baf6bd699e05d85b12847c74b
Albert Zeyer <albzey@gmail.com>
2020-08-10 00:41:17 +0200

    extend demo

--- tf-masked-softmax-ce-grad.py
+++ tf-masked-softmax-ce-grad.py
@@ -4,16 +4,37 @@ Demonstrate how to get some non-zero grad with ``0.0 * loss``.
 """
 
 import tensorflow as tf
+from tensorflow.python.framework import ops
 import better_exchook
+
+
+# noinspection PyUnusedLocal
+@ops.RegisterGradient("IdentityWithPrint")
+def _identity_with_print(op, grad):
+  with tf.control_dependencies([tf.print([op.name, "grad:", grad])]):
+    return [tf.identity(grad)]
+
+
+def debug_grad(x):
+  """
+  :param tf.Tensor x:
+  :return: x, but gradient will be printed
+  :rtype: tf.Tensor
+  """
+  g = tf.compat.v1.get_default_graph()
+  with g.gradient_override_map({"Identity": "IdentityWithPrint"}):
+    return tf.identity(x, name=x.name.split("/")[-1].replace(":", "_"))
 
 
 def main():
   max_seq_len = 15
   seq_len = 10
-  logits = tf.zeros([max_seq_len])
-  mask = tf.less(tf.range(max_seq_len), seq_len)
-  logits_masked = tf.where(mask, logits, float("-inf"))
 
+  logits = tf.zeros([max_seq_len])
+  logits_ = debug_grad(logits)
+
+  mask = tf.less(tf.range(max_seq_len), seq_len)
+  logits_masked = tf.where(mask, logits_, float("-inf"))
   ce = -tf.reduce_sum(tf.where(mask, tf.nn.softmax(logits_masked) * tf.nn.log_softmax(logits_masked), 0.0))
   loss = 0.0 * ce
 
