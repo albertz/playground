8d23366fe5ab601934c46d775304a325690e55eb
Albert Zeyer <albzey@gmail.com>
2021-08-20 21:32:54 +0200

    more

--- pycharm_super_getattr.py
+++ pycharm_super_getattr.py
@@ -4,7 +4,11 @@ https://youtrack.jetbrains.com/issue/PY-50410
 """
 
 
-class Obj:
+class Obj(object):
   def __getattr__(self, item):
     # Warning: Unresolved attribute reference '__getattr__' for class 'object'
     return super(Obj, self).__getattr__(item)
+
+
+a = Obj()
+print(a.foo)
