ea26b14b64fd3fafae69d6619093d7f09dc764b1
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2021-09-22 13:05:17 +0200

    PyCharm more
    https://youtrack.jetbrains.com/issue/PY-50828

--- pycharm-isinstance-type-var.py
+++ pycharm-isinstance-type-var.py
@@ -3,21 +3,40 @@
 from typing import TypeVar, Type, Iterable
 
 T = TypeVar('T')
+T_type = Type[T]
 
 
 # https://youtrack.jetbrains.com/issue/PY-31788
 def check_instance(obj, type_: Type[T]) -> T:
-    if isinstance(obj, type_):
-        return obj
+    assert isinstance(obj, type_)
     return None
+
+
+a_str = check_instance("foo", str)
+
+
+# https://youtrack.jetbrains.com/issue/PY-50828
+def check_instance_(obj, type_):
+    """
+    :param obj:
+    :param type[T] type_:
+    :rtype: T
+    """
+    assert isinstance(obj, type_)
+    return obj
+
+
+a_str_ = check_instance_("foo", str)
 
 
 # https://youtrack.jetbrains.com/issue/PY-32860
 def check_instance_tuple(obj, types: Iterable[Type[T]]) -> T:
     types = tuple(types)
-    if isinstance(obj, types):  # Type variables cannot be used with instance and class checks
-        return obj
-    return None
+    assert isinstance(obj, types)
+    return obj
+
+
+a_str__ = check_instance_tuple("foo", [str])
 
 
 class A:
