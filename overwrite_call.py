d7f6bf1288268529372026cf70f0436acb60cf62
Albert Zeyer <albzey@gmail.com>
2022-11-13 02:32:49 +0100

    test

---
+++ overwrite_call.py
@@ -0,0 +1,16 @@
+
+
+class Foo:
+  def __call__(self, x):
+    print("Original call")
+    return x * 2
+
+
+foo = Foo()
+foo(3)
+
+foo.__call__ = lambda x: print("Overwritten foo call") or x * 3
+foo(3)
+
+Foo.__call__ = lambda self, x: print("Overwritten Foo call") or x * 4
+foo(3)
