31f7d5714e01fdc09ba9eff1b626f4074e865d8e
Albert Zeyer <albzey@gmail.com>
2023-04-27 21:32:43 +0200

    more clear

--- tf_pt_stft.py
+++ tf_pt_stft.py
@@ -18,8 +18,6 @@ n_time = 16
 
 
 x = rnd.randn(n_batch, n_time)
-x_tf = tf.convert_to_tensor(x)
-x_pt = torch.from_numpy(x)
 
 
 frame_step = 1
@@ -29,26 +27,39 @@ fft_length = 8
 
 assert fft_length % 2 == 0 and frame_length % 2 == 0  # broken otherwise?
 
+x_tf = tf.convert_to_tensor(x)
 y_tf = tf.signal.stft(x, frame_step=frame_step, frame_length=frame_length, fft_length=fft_length)
 print(y_tf)
+y_tf_np = y_tf.numpy()
 
+x_pt = torch.from_numpy(x)
 x_pt = torch.nn.functional.pad(x_pt, (0, (fft_length - frame_length)))
 
 window_pt = torch.hann_window(frame_length)
 window_pt = torch.nn.functional.pad(window_pt, (0, (fft_length - frame_length)))
 
-y_pt = torch.stft(
+y_tf_like_pt = torch.stft(
     x_pt,
     n_fft=fft_length, hop_length=frame_step, win_length=fft_length,
     window=window_pt,
     center=False,
     return_complex=True,
 )
+y_tf_like_pt = y_tf_like_pt.transpose(1, 2)
+print(y_tf_like_pt)
+y_tf_like_pt_np = y_tf_like_pt.numpy()
+
+
+x_pt = torch.from_numpy(x)
+y_pt = torch.stft(
+    x_pt,
+    n_fft=fft_length, hop_length=frame_step, win_length=frame_length,
+    window=torch.hann_window(frame_length),
+    center=False,
+    return_complex=True,
+)
 y_pt = y_pt.transpose(1, 2)
 print(y_pt)
-
-
-y_tf_np = y_tf.numpy()
 y_pt_np = y_pt.numpy()
 
 
@@ -56,33 +67,47 @@ def ceildiv(a, b):
     return -(-a // b)
 
 
-y_np = numpy.zeros((n_batch, ceildiv(n_time - frame_length + 1, frame_step), fft_length // 2 + 1), dtype="complex64")
+y_tf_like_np = numpy.zeros(
+    (n_batch, ceildiv(n_time - frame_length + 1, frame_step), fft_length // 2 + 1), dtype="complex64")
 window_np = numpy.hanning(frame_length + 1)[:frame_length]
-for b in range(y_np.shape[0]):
-    for t in range(y_np.shape[1]):
-        for f in range(y_np.shape[2]):
+for b in range(y_tf_like_np.shape[0]):
+    for t in range(y_tf_like_np.shape[1]):
+        for f in range(y_tf_like_np.shape[2]):
             for k in range(frame_length):
-                # Case TF:
-                y_np[b, t, f] += (
-                        window_np[k]
-                        * x[b, t * frame_step + k]
-                        * numpy.exp(-2j * numpy.pi * f * k / fft_length))
-                # Case PT:
-                # y_np[b, t, f] += (
-                #         window_np[k]
-                #         * x[b, t * frame_step + k]
-                #         * numpy.exp(-2j * numpy.pi * f * (k + (fft_length - frame_length) // 2) / fft_length))
+                y_tf_like_np[b, t, f] += (
+                    window_np[k]
+                    * x[b, t * frame_step + k]
+                    * numpy.exp(-2j * numpy.pi * f * k / fft_length))
 
-print("Numpy:")
-print(y_np)
+print("TF-like in pure Numpy:")
+print(y_tf_like_np)
 
+y_pt_like_np = numpy.zeros(
+    (n_batch, ceildiv(n_time - fft_length + 1, frame_step), fft_length // 2 + 1), dtype="complex64")
+window_np = numpy.hanning(frame_length + 1)[:frame_length]
+for b in range(y_pt_like_np.shape[0]):
+    for t in range(y_pt_like_np.shape[1]):
+        for f in range(y_pt_like_np.shape[2]):
+            for k in range(frame_length):
+                y_pt_like_np[b, t, f] += (
+                    window_np[k]
+                    * x[b, t * frame_step + k + (fft_length - frame_length) // 2]
+                    * numpy.exp(-2j * numpy.pi * f * (k + (fft_length - frame_length) // 2) / fft_length))
+
+print("PT-like in pure Numpy:")
+print(y_pt_like_np)
 
 print("TF shape:", y_tf_np.shape)
-print("PT shape:", y_pt_np.shape)
-print("NP shape:", y_np.shape)
+print("TF-like in NP shape:", y_tf_like_np.shape)
+print("TF-like in PT shape:", y_tf_like_pt_np.shape)
 
-assert y_tf_np.shape == y_pt_np.shape == y_np.shape, (
-    f"TF shape {y_tf_np.shape} == PT shape {y_pt_np.shape} == NP shape {y_np.shape}")
-numpy.testing.assert_allclose(y_pt_np, y_np, rtol=1e-5, atol=1e-5, err_msg="PT != NP")
-numpy.testing.assert_allclose(y_tf_np, y_np, rtol=1e-5, atol=1e-5, err_msg="TF != NP")
-numpy.testing.assert_allclose(y_tf_np, y_pt_np, rtol=1e-5, atol=1e-5, err_msg="TF != PT")
+assert y_tf_np.shape == y_tf_like_pt_np.shape == y_tf_like_np.shape, (
+    f"TF shape {y_tf_np.shape} == TF-like PT shape {y_tf_like_pt_np.shape} == TF-like NP shape {y_tf_like_np.shape}")
+numpy.testing.assert_allclose(y_tf_np, y_tf_like_np, rtol=1e-5, atol=1e-5, err_msg="TF != TF-like NP")
+numpy.testing.assert_allclose(y_tf_np, y_tf_like_pt_np, rtol=1e-5, atol=1e-5, err_msg="TF != TF-like PT")
+
+print("PT shape:", y_pt_np.shape)
+print("PT-like in NP shape:", y_pt_like_np.shape)
+assert y_pt_np.shape == y_pt_like_np.shape, (
+    f"PT shape {y_pt_np.shape} != PT-like NP shape {y_pt_like_np.shape}")
+numpy.testing.assert_allclose(y_pt_np, y_pt_like_np, rtol=1e-5, atol=1e-5, err_msg="PT != PT-like NP")
