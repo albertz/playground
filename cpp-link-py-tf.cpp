ee01ee48b471d906ea90d1918a696789d8d66a9b
Albert Zeyer <albzey@gmail.com>
2023-06-02 00:48:54 +0200

    more

--- cpp-link-py-tf.cpp
+++ cpp-link-py-tf.cpp
@@ -3,10 +3,17 @@ see cpp-link-py-tf-compile.py
 */
 
 #include <Python.h>
+#include <tensorflow/core/public/session.h>
 
 int main() {
     Py_Initialize();
     PyImport_ImportModule("numpy");
+    PyImport_ImportModule("tensorflow");
+
+    tensorflow::GraphDef graph;
+    tensorflow::SessionOptions options;
+    auto session(tensorflow::NewSession(options));
+    tensorflow::Status s = session->Create(graph);
 
     return 0;
 }
