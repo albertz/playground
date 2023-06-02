8d7fd9a4ff4d71f40d5a9ab107fc7c7b310ba8a2
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2023-06-02 07:57:55 +0000

    cleanup

--- cpp-link-py-tf.cpp
+++ cpp-link-py-tf.cpp
@@ -2,17 +2,12 @@
 see cpp-link-py-tf-compile.py
 */
 
-#include <sys/resource.h>
 #include <Python.h>
 #include <tensorflow/core/public/session.h>
 
 int main() {
-    struct rlimit rl;
-    getrlimit(RLIMIT_STACK, &rl);
-    rl.rlim_cur = 64 * 1024 * 1024;   // 64MiB stack
-    setrlimit(RLIMIT_STACK, &rl);
-
     Py_Initialize();
+    PyImport_ImportModule("hashlib");
     PyImport_ImportModule("numpy");
     PyImport_ImportModule("tensorflow");
 
