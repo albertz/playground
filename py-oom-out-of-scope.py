0d8f59d272f7765a56d11b27b8a2aaede9ba9c00
Albert Zeyer <albzey@gmail.com>
2024-01-11 13:17:58 +0100

    python gc

---
+++ py-oom-out-of-scope.py
@@ -0,0 +1,67 @@
+
+import traceback
+import gc
+
+
+class Obj:
+    def __init__(self, name: str):
+        self.name = name
+
+    def __repr__(self):
+        return f"Obj({self.name!r})"
+
+    def __del__(self):
+        print("del", self)
+
+
+def deep(i: int):
+    a = Obj(f"a, i={i}")
+    if i == 2:
+        raise Exception(f"exception at i={i}")
+    print(a)
+
+
+def func():
+    prev_exc = None
+    for i in range(10):
+        if prev_exc:
+            # traceback.clear_frames(prev_exc.__traceback__)
+            clear_tb(prev_exc.__traceback__)
+            prev_exc = None
+        gc.collect()
+        print("** i:", i)
+        try:
+            deep(i)
+        except Exception as exc:
+            print("caught", exc)
+            print_tb(exc.__traceback__)
+            prev_exc = exc
+            continue  # continue with next i
+        print("func", i, "done")
+
+
+def print_tb(tb):
+    print("Call stack:")
+    while tb:
+        frame_i = tb.tb_frame.f_locals.get("i")
+        print(f"  {tb.tb_frame.f_code.co_name}: i={frame_i}")
+        tb = tb.tb_next
+
+
+def clear_tb(tb):
+    while tb:
+        print(tb.tb_frame.f_locals)
+        try:
+            tb.tb_frame.clear()
+        except RuntimeError:
+            print("  cannot clear?")
+        else:
+            print("  cleared")
+            # Using this code triggers that the ref actually goes out of scope, otherwise it does not!
+            # print("  now:", tb.tb_frame.f_locals)
+        tb = tb.tb_next
+
+
+if __name__ == '__main__':
+    func()
+    print("exit")
