680192ea15513b8914962f270411d7079358067a
Albert Zeyer <albzey@gmail.com>
2024-01-12 11:42:50 +0100

    more

--- py-oom-out-of-scope.py
+++ py-oom-out-of-scope.py
@@ -8,6 +8,11 @@ import traceback
 import gc
 
 
+_PrintTbSkipCurFrame = False
+_CallClearTb = True
+_ClearTbWithWorkaround = False
+
+
 class Obj:
     def __init__(self, name: str):
         self.name = name
@@ -35,23 +40,29 @@ def func():
         except Exception as exc:
             print("caught", exc)
             print_tb(exc.__traceback__)
-            # traceback.clear_frames(prev_exc.__traceback__)
-            clear_tb(exc.__traceback__)
+            if _CallClearTb:
+                # traceback.clear_frames(exc.__traceback__)
+                clear_tb(exc.__traceback__)
             continue  # continue with next i
         print("deep", i, "done")
 
 
 def print_tb(tb):
     print("Call stack:")
+    i = 0
     while tb:
-        frame_i = tb.tb_frame.f_locals.get("i")
-        print(f"  {tb.tb_frame.f_code.co_name}: i={frame_i}")
-        print("    locals is globals:", tb.tb_frame.f_locals is tb.tb_frame.f_globals)
-        print("    local var names:", tb.tb_frame.f_code.co_varnames, "num locals:", tb.tb_frame.f_code.co_nlocals)
+        if i == 0 and _PrintTbSkipCurFrame:
+            print("  (skip current frame)")
+        else:
+            frame_i = tb.tb_frame.f_locals.get("i")
+            print(f"  {tb.tb_frame.f_code.co_name}: i={frame_i}")
+            print("    locals is globals:", tb.tb_frame.f_locals is tb.tb_frame.f_globals)
+            print("    local var names:", tb.tb_frame.f_code.co_varnames, "num locals:", tb.tb_frame.f_code.co_nlocals)
 
         # if tb.tb_frame.f_code.co_nlocals > 0:
         #     tb.tb_frame.f_locals.clear()  # another alternative
         tb = tb.tb_next
+        i += 1
 
 
 def clear_tb(tb):
@@ -65,7 +76,8 @@ def clear_tb(tb):
         else:
             print("  cleared")
             # Using this code triggers that the ref actually goes out of scope, otherwise it does not!
-            # print("  now:", tb.tb_frame.f_locals)
+            if _ClearTbWithWorkaround:
+                print("  now:", tb.tb_frame.f_locals)
         tb = tb.tb_next
 
 
