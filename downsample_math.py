32a578a0d5876b18104b862daf94b8fb985fcabc
Albert Zeyer <albzey@gmail.com>
2024-02-09 23:27:27 +0100

    downsample math more

--- downsample_math.py
+++ downsample_math.py
@@ -9,6 +9,10 @@ def ceildiv(a, b):
     return (a + b - 1) // b
 
 
+def ceildiv_v2(a, b):
+    return (a - 1) // b + 1
+
+
 def wave_to_align_len_ref_peter(wave_len: int) -> int:
     feat_len = (wave_len - 200) // 80 + 1
     align_len = np.ceil(feat_len / 2)
@@ -20,8 +24,12 @@ def wave_to_align_len(wave_len: int) -> int:
     return (wave_len - 200) // (80 * 2 * 2) + 1
 
 
+def wave_to_align_len_v2(wave_len: int) -> int:
+    return ceildiv(wave_len - 200 + 1, 80 * 2 * 2)
+
+
 def test_wave_to_align_len():
     for i in range(0, 10_000):
-        a = wave_to_align_len(i)
+        a = wave_to_align_len_v2(i)
         b = wave_to_align_len_ref_peter(i)
         assert a == b, (i, a, b)
