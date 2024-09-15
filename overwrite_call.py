859b52d5e3d89a58dd8427ff1bebd10be4ff7f06
Albert Zeyer <zeyer@i6.informatik.rwth-aachen.de>
2024-09-15 23:44:19 +0200

    overwritten call

--- overwrite_call.py
+++ overwrite_call.py
@@ -1,16 +1,14 @@
-
-
 class Foo:
-  def __call__(self, x):
-    print("Original call")
-    return x * 2
+    def __call__(self, x):
+        print("Original call")
+        return x * 2
 
 
 foo = Foo()
 foo(3)
 
 foo.__call__ = lambda x: print("Overwritten foo call") or x * 3
-foo(3)
+foo(3)  # still original call!
 
 Foo.__call__ = lambda self, x: print("Overwritten Foo call") or x * 4
-foo(3)
+foo(3)  # now overwritten call
