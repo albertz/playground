4c3a6901c4406f0d805a6a7e4ed1886d10f97ea0
Albert Zeyer <albzey@gmail.com>
2025-04-02 15:58:25 +0200

    some cleanup

--- torch-dyn-shape.py
+++ torch-dyn-shape.py
@@ -8,7 +8,7 @@ import sympy
 # noinspection PyProtectedMember
 from torch._subclasses.fake_tensor import FakeTensorMode
 
-from torch.fx.experimental.symbolic_shapes import ShapeEnv, DimDynamic, SymInt
+from torch.fx.experimental.symbolic_shapes import ShapeEnv
 
 # noinspection PyProtectedMember
 from torch._dynamo.source import ConstantSource
@@ -18,12 +18,10 @@ from torch._dynamo.source import ConstantSource
 m = torch.nn.ConvTranspose1d(1, 1, 1, stride=5, padding=3)
 
 
-shape_env = ShapeEnv()
+shape_env = ShapeEnv(duck_shape=False)
 with FakeTensorMode(allow_non_fake_inputs=True, shape_env=shape_env) as fake_tensor_mode:
-    # time_sym = shape_env.create_symbol(100, ConstantSource("T"))
-    time_src = ConstantSource("T")
-    time_sym = shape_env.create_symbol(100, time_src, dynamic_dim=DimDynamic.DYNAMIC)
-    time_sym_ = shape_env.create_symintnode(time_sym, hint=100, source=time_src)
+    time_sym = shape_env.create_symbol(100, ConstantSource("T"))
+    time_sym_ = shape_env.create_symintnode(time_sym, hint=None)
 
     x = torch.empty((1, 1, time_sym_))
     y = m(x)
@@ -31,7 +29,6 @@ with FakeTensorMode(allow_non_fake_inputs=True, shape_env=shape_env) as fake_ten
     print(f"{y.shape=}")
     out_size = y.shape[-1]
     print(f"{out_size=} {type(out_size)=}")
-    assert isinstance(out_size, SymInt)
     print(f"{out_size.node=} {type(out_size.node)=}")
     print(f"{out_size.node.expr=} {type(out_size.node.expr)=}")
     out_sym = out_size.node.expr
